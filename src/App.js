6baab3e74974c915582ca07e1fafb8d0c4922099
seerde <mr.abod7oom@gmail.com>
2020-10-31 21:38:04 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+  Card,
+  CardDeck,
+} from "react-bootstrap";
+
+import Edrees from "./Edrees";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {[...Array(100).keys()].map((element) => (
+        <Edrees />
+      ))}
     </div>
   );
 }
---
+++ src/Edrees.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
+
+const Edrees = () => {
+  return (
+    <div>
+      <Container>
+        <Row className="justify-content-md-center">
+          <h1>
+            my name is <span>Edrees</span>
+          </h1>
+        </Row>
+        <Row>
+          <p>
+            Lorem ipsum dolo Lorem ipsum doloLorem ipsum doloLorem ipsum
+            doloLorem ipsum doloLorem ipsum doloLorem ipsum doloLorem ipsum
+            doloLorem ipsum doloLorem ipsum doloLorem ipsum doloLorem ipsum
+            doloLorem ipsum doloLorem ipsum doloLorem ipsum doloLorem ipsum
+            doloLorem ipsum doloLorem ipsum doloLorem ipsum doloLorem ipsum
+            doloLorem ipsum doloLorem ipsum doloLorem ipsum doloLorem ipsum
+            doloLorem ipsum doloLorem ipsum doloLorem ipsum doloLorem ipsum
+            doloLorem ipsum doloLorem ipsum doloLorem ipsum doloLorem ipsum
+            doloLorem ipsum doloLorem ipsum doloLorem ipsum doloLorem ipsum
+            doloLorem ipsum doloLorem ipsum dolos
+          </p>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Edrees;
